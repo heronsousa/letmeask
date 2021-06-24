785ed9053eb5a66f0fb41df174aa87146e96b381
Heron Rodrigues <heronrs1@gmail.com>
2021-06-23 22:07:59 -0300

    Styling home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,12 @@ import illustration from '../assets/images/illustration.svg'
 import logo from '../assets/images/logo.svg'
 import googleIcon from '../assets/images/google-icon.svg'
 
+import '../styles/home.scss'
+import { Button } from './../components/Button';
+
 export function Home() {
     return(
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustration} alt="Perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
@@ -12,16 +15,16 @@ export function Home() {
             </aside>
 
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logo} alt="Letmeask" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleIcon} alt="" />
                         Crie sua sala com o Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className="separator">ou entre em uma sala</div>
                     <form>
                         <input type="text" placeholder="Digite o código da sala" />
-                        <button type="submit">Entrar na sala</button>
+                        <Button type="submit">Entrar na sala</Button>
                     </form>
                 </div>
             </main>
