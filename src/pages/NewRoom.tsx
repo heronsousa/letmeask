e8a234b793ac2693e127bc17876b3fb6b2eb087e
Heron Rodrigues <heronrs1@gmail.com>
2021-06-24 22:34:29 -0300

    Create new room

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { FormEvent } from 'react';
+import { useState } from 'react';
+
+import { database } from '../services/firebase';
+import { useAuth } from './../hooks/useAuth';
 
 import { Button } from './../components/Button';
 import illustration from '../assets/images/illustration.svg'
 import logo from '../assets/images/logo.svg'
-
 import '../styles/auth.scss'
-import { AuthContext } from '../App';
-import { useContext } from 'react';
 
 export function NewRoom() {
-    const { user, signInWithGoogle } = useContext(AuthContext);
+    const { user } = useAuth();
+    const [newRoom, setNewRoom] = useState('')
+
+    const history = useHistory();
+    
+    async function handleCreateRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (newRoom.trim() === '') return
+
+        const roomRef = database.ref('rooms');
+
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        });
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
 
     return(
         <div id="page-auth">
@@ -24,8 +44,13 @@ export function NewRoom() {
                     <img src={logo} alt="Letmeask" />
                     <h1>{user?.name}</h1>
                     <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input type="text" placeholder="Nome da sala" />
+                    <form onSubmit={handleCreateRoom}>
+                        <input 
+                            type="text" 
+                            placeholder="Nome da sala" 
+                            onChange={event => setNewRoom(event.target.value)}
+                            value={newRoom}
+                        />
                         <Button type="submit">Criar sala</Button>
                     </form>
                     <p>
